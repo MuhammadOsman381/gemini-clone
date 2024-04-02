b750934dd28d61b69ad64cce4195383d7b457739
Muhammad Osman <139002050+MuhammadOsman381@users.noreply.github.com>
2024-04-02 15:57:58 +0500

    adding animations

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import { useState } from 'react';
-// import Input from './input';
+import {motion} from 'framer-motion';
 import { useEffect } from 'react';
-// import Input from './input';
-// import Greeting from './greeting';
 
 function Main(props){
     
-    const [para,setPara] = useState('');
+
     const [prompt, setPrompt] = useState(null);
     const [updated,setUpdated] = useState('');
     
@@ -27,34 +25,27 @@ function Main(props){
             data:{contents:[{parts:[{text:prompt}]}]}
          })
 
-        //  console.log(response.data);
-
-         setPara(response.data.candidates[0].content.parts[0].text);
-         
-            console.log(response.data.candidates[0].content.parts[0].text)
+       
     
          document.querySelector('#greet').style.display = 'none';
          document.querySelector('.paragraph').style.display = 'flex'
          document.querySelector('.prompt').style.display = 'flex'
 
          const paragraphs = response.data.candidates[0].content.parts[0].text.split('\n\n');
-        //  alert('Please wait a moment until the data has been fetched.');
-         const u = paragraphs.map((paragraph, index) => {
-            // Check if the paragraph contains a heading (marked with "**")
+        
+         const newpara = paragraphs.map((paragraph, index) => {
+          
             if (paragraph.startsWith('**') && paragraph.endsWith('**')) {
-              // Render heading with bold and larger font size
+            
               return <h2 className='updatedhead' key={index} style={{ fontWeight: 'bold', fontSize: '1.3rem' }}>{paragraph.replace(/\*\*/g, '')}</h2>;
             } else {
-              // Render regular paragraph with space between paragraphs
-            //   if(paragraph.endsWith('*')){
-                
-            //   }
+            
               return <p className='updatedpara' key={index} style={{ marginBottom: '1rem' }}>{paragraph.replace(/\*\*/g, '')}</p>;
             }
           
           })
 
-          setUpdated(u)
+          setUpdated(newpara)
       };
 
      useEffect(()=>{
@@ -67,27 +58,27 @@ function Main(props){
         
         <div className=" w-[100vw] h-[86vh]  flex flex-col  items-center justify-center  m-[0px] p-[0px] gap-[1vw] max-sm:h-[80vh] max-sm:gap-[1vh]">
              
-             <div id="greet" className="  w-[100vw] h-[75vh] flex flex-col  items-center justify-center  m-[0px] p-[0px]  max-sm:h-[65vh] ">
+             <div id="greet" className="  w-[100vw] h-[70vh] flex flex-col items-left  justify-center  m-[0px] p-[0px]  max-sm:h-[65vh] ">
             <div className=" flex flex-col m-[0px] w-auto h-[28vh] items-center justfy-center ">
-            <h1 className="text-[4.5vw] font-bold text-white max-sm:text-[4.5vh]  h-auto max-sm:w-[35vh] " id="text-gradient  ">Hello,</h1>
-            <h1 className=" text-[4.5vw] font-bold text-[#444746] max-sm:text-[4.5vh]  h-auto  max-sm:w-[35vh]">How can I help you today?</h1>
+            <motion.h1 animate ={{x:[0,-40,0]}} transition={{duration:"3", delay:"0"}} className="text-[4.5vw] font-bold text-white max-sm:text-[4.5vh]  h-auto max-sm:w-[35vh] text-left  w-[57vw]" id="text-gradient  ">Hello,</motion.h1>
+            <motion.h1 animate ={{x:[0,-40,0]}} transition={{duration:"4", delay:"0"}} className=" text-[4.5vw] font-bold text-[#444746] max-sm:text-[4.5vh]  h-auto  max-sm:w-[35vh]">How can I help you today?</motion.h1>
             </div>
             
         </div>
            
-        <div className='prompt h-[7vh] w-[50vw]  hidden flex max-sm:w-[45vh] max-sm:h-[5vh]'>
+        <motion.div animate ={{y:[0,-10,0]}} transition={{duration:"3", delay:"0"}} className='prompt h-[7vh] w-[50vw]   hidden flex max-sm:w-[45vh] max-sm:h-[5vh]'>
             <strong><h1 className='h-auto w-auto text-[1.7vw] max-sm:text-[2.5vh] '>{prompt}</h1></strong>
-        </div>
-         <div className='paragraph overflow-scroll h-[70vh] w-[50vw] text-[3.2vh] gap-[0vw] mt-[0vw] flex flex-col   hidden  max-sm:w-[45vh] max-sm:h-[60vh] '>
+        </motion.div>
+         <motion.div animate ={{y:[0,-10,0]}} transition={{duration:"4", delay:"0"}}  className='paragraph overflow-scroll h-[60vh] w-[50vw] text-[3.2vh] gap-[0vw] mt-[0vw] flex flex-col   hidden  max-sm:w-[45vh]  max-sm:h-[60vh] '>
                   
             {updated}
         
 
-         </div>
+         </motion.div>
     
         <div className=' h-auto w-auto'>
-        <input type="text" className="text border-none w-[45vw] h-[9vh]  bg-[#444746] text-[#b5b9be] outline-none p-[1.5vw] max-sm:h-[7vh] max-sm:w-[36vh] max-sm:p-[2vh]" onChange={handler} placeholder="Enter a prompt here" />
-           <button className="btn  w-[14vh] bg-white text-black h-[9vh] rounded-[30px] max-sm:h-[7vh] max-sm:p-[2vh] max-sm:w-[10vh]"  onClick={data}> submit</button>
+        <input type="text" className="text -none w-[45vw] h-[9vh]  bg-[#444746] text-[#b5b9be] outline-none p-[1.5vw] max-sm:h-[7vh] max-sm:w-[36vh] max-sm:p-[2vh]" onChange={handler} placeholder="Enter a prompt here" />
+           <button  className="btn  w-[14vh] bg-white text-black h-[9vh]  max-sm:h-[7vh] max-sm:p-[2vh] max-sm:w-[10vh]"  onClick={data}> submit</button>
         </div>
         
         </div>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,12 @@
 import img1 from './logo.png'
-
+import {motion} from 'framer-motion';
 function Nav(){
     return(
         <div className=" border-b h-[11vh] w-auto flex items-center p-[4vh] max-sm:h-[0vh]">
-            <h1 className=" w-auto h-auto">
+            <motion.h1  animate ={{y:[0,-15,0]}} transition={{duration:"3", delay:"0"}} className=" w-auto h-auto">
                 {/* <strong className="text-[1.7vw] ">Gemini</strong>  */}
-                <img src={img1} alt="" className='w-[7vw] max-sm:w-[9vh]' />
-            </h1>
+                <img src={img1} alt="" className='w-[6vw] max-sm:w-[9vh]' />
+            </motion.h1>
         </div>
     )
 }
