7c8ce1698d258aacf9d46dd55eb0756016febe97
Muhammad Osman <139002050+MuhammadOsman381@users.noreply.github.com>
2024-04-07 00:36:17 +0500

    respo hase been done

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -99,35 +99,25 @@ function Main(props) {
   return (
 
 
-    <div className="   w-[100vw] h-[88vh]  flex flex-col  items-center justify-center  m-[0px] p-[0px] gap-[0vw] max-sm:h-[80vh] max-sm:gap-[0vh]" >
-      <div className='flex flex-row w-auto h-auto gap-[0vw]'>
-        {
-        <div className=' w-[56vw] h-[70vh] flex flex-col hidden' id='parent'>
-               {loader ? 
-               ( <Loader/> )
-               :  prearray.map((item) => (
-                <div className='preprompt'    key={item}>
-                  {item.prompts}
-                </div>
-              ))}
-          
-        </div>}
-        <div id="greet" className="   w-[100vw] h-[70vh] flex flex-col items-center  justify-center  m-[0px] p-[0px]  max-sm:h-[65vh] ">
-          <div className=" flex flex-col m-[0px] w-auto h-[28vh] items-center justfy-center ">
+    <div className="  w-[100vw] h-[88.5vh]  flex flex-col items-center justify-center  m-[0px] p-[0px] gap-[0%] max-sm:h-[80vh] max-sm:gap-[0vh]" >
+      <div className='-green-200 flex flex-row w-auto h-auto gap-[0vw]'>
+     
+        <div id="greet" className=" w-[100vw] h-[63vh] flex flex-col items-center  justify-center  m-[0px] p-[0px]  max-sm:h-[65vh] ">
+          <div className=" flex flex-col m-[0px] w-auto h-auto items-center justfy-center ">
             <motion.h1 animate={{ x: [0, -40, 0] }} transition={{ duration: "3", delay: "0" }} className="text-[4.5vw] font-bold text-white max-sm:text-[4.5vh]  h-auto max-sm:w-[35vh] text-left  w-[57vw]" id="text-gradient  ">Hello,</motion.h1>
             <motion.h1 animate={{ x: [0, -40, 0] }} transition={{ duration: "4", delay: "0" }} className=" text-[4.5vw] font-bold text-[#444746] max-sm:text-[4.5vh]  h-auto  max-sm:w-[35vh]">How can I help you today?</motion.h1>
           </div>
 
-        </div>
+        </div> 
 
-        <div className='flex flex-col w-auto h-auto'>
-          <motion.div animate={{ y: [0, -10, 0] }} transition={{ duration: "3", delay: "0" }} className=' prompt h-[7vh] w-[75vw]   hidden flex max-sm:w-[45vh] max-sm:h-[5vh] max-sm:mt-[2vh]'>
-            <strong><h1 className='h-auto w-auto text-[1.7vw] max-sm:text-[2.5vh] '>{prompt}</h1></strong>
+      <div className=' flex flex-col items-center justify-center w-auto h-auto mt-[0vw]'>
+          <motion.div animate={{ y: [0, -10, 0] }} transition={{ duration: "3", delay: "0" }} className=' prompt h-[15%] w-[75vw]   hidden flex max-sm:w-[45vh] max-sm:h-[5vh] max-sm:mt-[2vh]'>
+            <strong><h1 className='h-auto w-auto text-[1.4rem] max-sm:text-[2.5vh] '>{prompt}</h1></strong>
           </motion.div>
 
       
 
-          <motion.div animate={{ y: [0, -10, 0] }} transition={{ duration: "4", delay: "0" }} className='  paragraph overflow-scroll h-[63vh] w-[75vw] text-[3.2vh] gap-[0vw] mt-[0vw] flex flex-col   hidden  max-sm:w-[45vh]  max-sm:h-[60vh] '>
+          <motion.div animate={{ y: [0, -10, 0] }} transition={{ duration: "4", delay: "0" }} className='  paragraph overflow-scroll h-[63vh] w-[75vw] text-[3.2vh] gap-[0vw]  flex flex-col   hidden  max-sm:w-[45vh]  max-sm:h-[60vh] '>
           {loader ? 
                ( <Loader/> )
                : updated
@@ -135,11 +125,11 @@ function Main(props) {
            
    
           </motion.div>
-        </div>
+        </div> 
       </div>
       <div className=' border-t h-[20vh] w-[100vw] flex flex-row items-center justify-center max-sm:h-[15vh]'>
-        < motion.input animate={{ y: [0, -20, 0] }} transition={{ duration: "4", delay: "0" }} type="text" className="text -none w-[45vw] h-[9vh]  bg-[#444746] text-[#b5b9be] outline-none p-[1.5vw] max-sm:h-[7vh] max-sm:w-[36vh] max-sm:p-[2vh]" onChange={handler} placeholder="Enter a prompt here" />
-        <motion.button type='submit' animate={{ y: [0, -20, 0] }} transition={{ duration: "4", delay: "0" }} className="btn  w-[14vh] bg-white text-black h-[9vh]  max-sm:h-[7vh] max-sm:p-[2vh] max-sm:w-[10vh]" onClick={data}> submit</motion.button>
+        < motion.input animate={{ y: [0, -20, 0] }} transition={{ duration: "4", delay: "0" }} type="text" className="text -none w-[45vw] h-[3.5rem]  bg-[#444746] text-[#b5b9be] outline-none p-[1.5vw] max-sm:h-[7vh] max-sm:w-[36vh] max-sm:p-[2vh]" onChange={handler} placeholder="Enter a prompt here" />
+        <motion.button type='submit' animate={{ y: [0, -20, 0] }} transition={{ duration: "4", delay: "0" }} className="btn  w-[5rem] bg-white text-black h-[3.5rem]  max-sm:h-[7vh] max-sm:p-[2vh] max-sm:w-[10vh]" onClick={data}> submit</motion.button> 
       </div>
 
     </div>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,11 +5,11 @@ import Main from './main';
 function Nav(props){
     const divRef = useRef(null);
     return(
-        <div className=" border-b h-[11vh] w-[100vw] mt-[-4vh]  flex items-center p-[4vh] max-sm:h-[3vh] max-sm:mt-[-3vh] gap-[2vh]">
+        <div className=" border-b h-[4rem] w-[100vw] mt-[-6vh]  flex items-center p-[4vh] max-sm:h-[3vh] max-sm:mt-[-3vh] gap-[2vh]">
 
             <motion.div  animate ={{y:[0,-15,0]}} transition={{duration:"3", delay:"0"}} className=" w-auto h-auto">
      
-                <img src={img1} alt="" className='w-[6vw] max-sm:w-[9vh]' />
+                <img src={img1} alt="" className='w-[6rem] max-sm:w-[9vh]' />
             </motion.div>
            
         </div>
